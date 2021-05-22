31f4c4b0d78c2daf87680974893d59c9b1c8c906
fladdimir <fladdi.mir@gmx.de>
2021-05-22 16:06:40 +0200

    bookshelf

---
+++ src/main/java/org/demo/bookshelf/BookRepository.java
@@ -0,0 +1,9 @@
+package org.demo.bookshelf;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    
+}
